953f858852d0dd0b2bb243478bf125a132098861
Olivier JM Maniraho <manolivier93@gmail.com>
2020-06-03 17:42:08 +0200

    updated visitors to add uuid attribute

--- lib/search_cop/visitors/visitor.rb
+++ lib/search_cop/visitors/visitor.rb
@@ -82,6 +82,7 @@ module SearchCop
       alias :visit_SearchCopGrammar_Attributes_Date :visit_attribute
       alias :visit_SearchCopGrammar_Attributes_Time :visit_attribute
       alias :visit_SearchCopGrammar_Attributes_Boolean :visit_attribute
+      alias :visit_SearchCopGrammar_Attributes_Uuid :visit_attribute
 
       def quote(value)
         connection.quote value
@@ -96,6 +97,7 @@ module SearchCop
       alias :visit_Fixnum :quote
       alias :visit_Symbol :quote
       alias :visit_Integer :quote
+      alias :visit_Uuid :quote
 
       # rubocop:enable Naming/MethodName
     end
